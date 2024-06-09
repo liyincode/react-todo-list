99ff392102b3e0b87f2e79ba8273b9433de34046
liyin <liyincode@gmail.com>
2024-06-09 16:08:44 +0800

    feat: save todos by localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { AddToDo } from './components/AddToDo.tsx'
 import { Header } from './components/Header.tsx'
 import { ToDoList } from './components/ToDoList.tsx'
 import type { ToDo } from './types.ts'
 
 function App() {
-  const [todos, setTodos] = useState<ToDo[]>([])
+  const [todos, setTodos] = useState<ToDo[]>(() => {
+    const saved = localStorage.getItem('todos')
+    if (saved) {
+      return JSON.parse(saved)
+    }
+    return []
+  })
+
+  useEffect(() => localStorage.setItem('todos', JSON.stringify(todos)), [todos])
 
   function handleAdd(todo: ToDo) {
     setTodos([...todos, todo])
